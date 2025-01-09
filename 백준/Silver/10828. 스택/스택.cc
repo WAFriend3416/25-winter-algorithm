6dfa0e25fe2689c59eac9dd86ba4045d4842478f
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2025-01-09 15:28:13 +0900

    [Silver IV] Title: 스택, Time: 220 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+#include <stack>
+
+using namespace std;
+
+int main(){
+    stack<int> s;
+    int cmd_cnt;
+    cin >> cmd_cnt;
+
+    while(cmd_cnt--){
+        string cmd;
+        cin >> cmd;
+        if(cmd == "push"){
+            int value;
+            cin >> value;
+            s.push(value);
+        }
+        else if (cmd == "pop")
+        {
+            if(s.empty()) cout << "-1" << endl;
+            else {
+                cout << s.top() << endl;
+                s.pop();
+            }
+            
+        }
+        else if (cmd == "size")
+        {
+            cout << s.size() << endl;
+        }
+        else if (cmd == "empty")
+        {
+            if(s.empty()) cout << "1" << endl;
+            else cout << "0" << endl;
+        }
+        else if (cmd == "top")
+        {
+            if(s.empty()) cout << "-1" << endl;
+            else cout << s.top() << endl;
+        }
+    }
+
+    return 0;
+}
