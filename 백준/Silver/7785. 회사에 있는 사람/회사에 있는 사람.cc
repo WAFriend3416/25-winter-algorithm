6d17f9d6da7af29a5a59076c54db4b63a548766f
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2025-02-12 15:42:23 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 76 ms, Memory: 12884 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -1,28 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <unordered_set>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
-unordered_set<string> s;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    unordered_set<string> s;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  int N;
-  cin >> N;
-  while(N--){
-    string name, log;
-    cin >> name >> log;
-    if(log == "enter"){
-        s.insert(name);   
-    } 
-    else{
-        s.erase(name);    
-    } 
-  }
+    while(N--){
+        string name;
+        string inout;
+        cin >> name;
+        cin >> inout;
+        if(inout == "enter") s.insert(name);       
+        else s.erase(name);
+    }
+    vector<string> v(s.begin(),s.end());
+    sort(v.begin(),v.end(),greater<string>());
     
-  vector<string> ans(s.begin(), s.end());
-  sort(ans.begin(), ans.end(), greater<string>());
-    
-  for(auto x : ans) cout << x << '\n';
-    
+    for(auto ch : v) cout << ch << "\n";
+    return 0;
 }
